4c0fb6c35f109dec3cbeb2e3b41c76337b620ce5
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2022-03-16 19:34:07 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+bool? value = false;
+//Sometimes we get a warning here
+var a = value switch
+{
+    null => "s",
+    false => "aa",
+    true => "aa"
+};
+
+namespace SwitchTest
+{
+    public class Test
+    {
+        public bool? Value { get; set; }
+        public override string ToString()
+        {
+            //we always get warnings here
+            var a = Value switch
+            {
+                null => nameof(Value),
+                false => "aa",
+                true => "aa"
+            };
+            return a;
+        }
+
+        public string S
+        {
+            get
+            {
+                //and here
+                return Value switch
+                {
+                    null => nameof(Value),
+                    false => "aa",
+                    true => "aa"
+                };
+            }
+        }
+    }
+}
